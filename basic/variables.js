40653b9f86dc6adb12d5487024483fcc2000c124
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2025-01-24 21:17:14 +0530

    Create variables.js

---
+++ basic/variables.js
@@ -0,0 +1,13 @@
+const accountId = 65865; //we create a accountId constant which is never be change
+let accuntEmail = "vanshdh34@google.com"; // let is use most of the time in code to allocate space to variable
+var accountPassword = "shi";
+accountCity = "Indore"; // ti`s also a type of allocated a variable in code
+let accountstate;
+
+accuntEmail = "h@klsd.com" // we change the variables value
+accountPassword = "van"
+accountCity = "mumbai";
+
+console.log(accountId);
+
+console.table([accountId , accountPassword, accuntEmail ,accountCity ,accountstate]);
