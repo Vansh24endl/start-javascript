b3fe41f9ddd3f9ee1f42e7b6fd79df7f802d5b0e
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2025-01-26 20:20:54 +0530

    Create Airthmaticeq.js

---
+++ basic/Airthmaticeq.js
@@ -0,0 +1,19 @@
+// Function to add two numbers
+let add = (a, b) => (a + b);
+console.log(add(17, 5));
+
+// Function to subtract the second number from the first
+let subt = (a, b) => (a - b);
+console.log(subt(25, 6));
+
+// Function to multiply two numbers
+let multiply = (a, b) => (a * b);
+console.log(multiply(4, 5));
+
+// Function to divide the first number by the second
+let divide = (a, b) => (a / b);
+console.log(divide(20, 4));
+
+// Function to find the remainder of the division of the first number by the second
+let modulus = (a, b) => (a % b);
+console.log(modulus(22, 6));
