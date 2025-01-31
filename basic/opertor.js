9df8ddd6e1676ebe4331450238ddfc820162ba21
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2025-01-31 20:07:08 +0530

    Create opertor.js

---
+++ basic/opertor.js
@@ -0,0 +1,25 @@
+console.log("2" > 1);
+console.log("2" < 1);
+console.log("2" >> 1);
+console.log("2" << 1);
+console.log("2" >= 1);
+console.log("2" <= 1);
+console.log("2" == 1);
+console.log("2" === 1);   // strictliy check the datatype
+console.log("2" != 1);
+console.log("2" !== 1);
+console.log("2" >>> 1);  // right shift operator
+
+// output 
+/*  true
+*false
+*1
+*4
+*true
+*false
+*false
+*false
+*true
+*true
+*1 
+*/
