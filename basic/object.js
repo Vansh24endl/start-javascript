7ebd1d3a572f23ed7aa9232efbf64a40bba71760
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2025-02-02 19:20:07 +0530

    Update basic

---
+++ basic/object.js
@@ -0,0 +1,5 @@
+// Create an Object:
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+// Display Data from the Object:
+console.log(person.firstName + " is " + person.age + " years old.");
